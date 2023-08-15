b4ba019f6e3d07087419e4486373c39b2b046526
Matheus Pereira <mat_pereiraa@hotmail.com>
2023-08-15 10:23:21 -0300

    refactor: change name to axios-http-adapter and return response correct values

---
+++ src/infra/http/axios-http-adapter/axios-http-adapter.ts
@@ -0,0 +1,13 @@
+import { HttpPostClient, HttpPostParams } from "@/data/protocols/http/http-post-client";
+import { HttpResponse } from "@/data/protocols/http/http-response";
+import axios from 'axios'
+
+export class AxiosHttpAdapter implements HttpPostClient<any, any>{
+  async post (params : HttpPostParams<any>) : Promise<HttpResponse<any>> {
+    const response = await axios.post(params.url, params.body)
+    return {
+      statusCode: response.status,
+      body: response.data
+    }
+  }
+}
