88bee60bf1097ddfa769a0b15b27fc6d5ebc23a2
Matheus Pereira <mat_pereiraa@hotmail.com>
2023-08-16 09:53:54 -0300

    feat: ensure login starts with input with required state

--- src/presentation/components/Input/index.tsx
+++ src/presentation/components/Input/index.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import Styles from "./styles.scss";
+import Context from "@/presentation/contexts/form/formContext";
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 function Input(props : Props) {
+
+  const {errorState} = useContext(Context)
+  const error = errorState[props.name]
+
+  const getStatus = () : string => {
+    return '🔴'
+  }
+
+  const getError = () : string => {
+    return error
+  }
+
   return (
     <div className={Styles.inputWrap}>
       <input {...props} readOnly />
-      <span className={Styles.status}>🔴</span>
+      <span data-testid={`${props.name}-status`} title={getError()} className={Styles.status}>{getStatus()}</span>
     </div>
   );
 }
--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -4,23 +4,23 @@ import Header from "@/presentation/components/LoginHeader";
 import {Footer, Input, FormStatus } from "@/presentation/components";
 import Context from '@/presentation/contexts/form/formContext';
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
-
 
 const Login : React.FC = () => {
 
-  const [state, setState] = useState<StateProps>({
-    isLoading: false,
-    errorMessage: ''
+  const [state, setState] = useState({
+    isLoading: false
+  })
+
+  const [errorState] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
   })
 
   return (
     <div className={Styles.login}>
       <Header />
-      <Context.Provider value={state}>
+      <Context.Provider value={{state, errorState}}>
         <form className={Styles.form}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
